f53d60008c08e215d756e659859c76aa459856ec
Arsy Shifa Nuril Azmi <email@arsyshifa06062003.com>
2024-12-23 22:40:19 +0700

    katalog commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,53 +28,119 @@
         <p>Get The Best Dimsum in Town</p>
     </section>
 
-    <h1 class="pheading">Our Products</h1>
+    <h1 class="pheading">Katalog Produk</h1>
+    <h2 class="pheading">Produk Matang</h2>
 
     <section class="sec">
         <div class="products">
 
             <div class="card">
-                <div class="img"><img src="{{Vite::asset('resources/img/Produk 1.jpeg')}}" alt=""></div>
-                <div class="dexc">Molen</div>
-                <div class="title">Molen</div>
+                <div class="img"><img src="{{Vite::asset('resources/img/Produk 5.jpg')}}" alt=""></div>
+                <h3>Dimsum Original 5 pcs</h3>
+                <br>
+                <div class="title">Dimsum Original dengan 100% daging ayam yang disajikan hangat</div>
                 <div class="box">
-                    <div class="price">RP.15000</div>
-                    <button class="btn">Buy Now</button>
+                    <div class="price">Rp.16.000</div>
+                
                 </div>
             </div>
             <div class="card">
-                <div class="img"><img src="{{Vite::asset('resources/img/Produk 1.jpeg')}}" alt=""></div>
-                <div class="dexc">Molen</div>
-                <div class="title">Molen</div>
+                <div class="img"><img src="{{Vite::asset('resources/img/Produk 4.jpeg')}}" alt=""></div>
+                <h3>Dimsum Keju 5 pcs</h3>
+                <br>
+                <div class="title">Dimsum Keju terbuat dari 100% daging ayam dengan keju diatasnya yang disajikan hangat</div>
                 <div class="box">
-                    <div class="price">RP.15000</div>
-                    <button class="btn">Buy Now</button>
+                    <div class="price">Rp.17.000</div>
+                    {{-- <button class="btn">Buy Now</button> --}}
                 </div>
             </div>
             <div class="card">
-                <div class="img"><img src="{{Vite::asset('resources/img/Produk 1.jpeg')}}" alt=""></div>
-                <div class="dexc">Molen</div>
-                <div class="title">Molen</div>
+                <div class="img"><img src="{{Vite::asset('resources/img/Produk 6.jpg')}}" alt=""></div>
+                <h3>Dimsum Crab 5 pcs</h3>
+                <br>
+                <div class="title">Dimsum Crab terbuat dari 100% daging ayam dengan potongan crabstick diatasnya yang disajikan hangat</div>
                 <div class="box">
-                    <div class="price">RP.15000</div>
-                    <button class="btn">Buy Now</button>
+                    <div class="price">Rp.17.000</div>
+                    {{-- <button class="btn">Buy Now</button> --}}
                 </div>
             </div>
-            <div class="card">
-                <div class="img"><img src="{{Vite::asset('resources/img/Produk 1.jpeg')}}" alt=""></div>
-                <div class="dexc">Molen</div>
-                <div class="title">Molen</div>
-                <div class="box">
-                    <div class="price">RP.15000</div>
-                    <button class="btn">Buy Now</button>
-                </div>
-            </div>
+        </div>
+    </section>
+    <h2 class="pheading">Produk Frozen</h2>
+    <section class="sec">
+        <div class="products">
 
+            <div class="card">
+                <div class="img"><img src="{{Vite::asset('resources/img/Produk 7.jpg')}}" alt=""></div>
+                <h3>Dimsum Original 50 pcs</h3>
+                <br>
+                <div class="title">Dimsum Original dengan 100% daging ayam tersedia dalam bentuk frozen food</div>
+                <div class="box">
+                    <div class="price">Rp.60.000</div>
+                    {{-- <button class="btn">Buy Now</button> --}}
+                </div>
+            </div>
+            <div class="card">
+                <div class="img"><img src="{{Vite::asset('resources/img/Produk 9.jpg')}}" alt=""></div>
+                <h3>Dimsum Keju 50 pcs</h3>
+                <br>
+                <div class="title">Dimsum Keju terbuat dari 100% daging ayam dengan keju diatasnya tersedia dalam bentuk frozen food</div>
+                <div class="box">
+                    <div class="price">Rp.63.000</div>
+                    {{-- <button class="btn">Buy Now</button> --}}
+                </div>
+            </div>
+            <div class="card">
+                <div class="img"><img src="{{Vite::asset('resources/img/Produk 8.jpg')}}" alt=""></div>
+                <h3>Dimsum Crab 50 pcs</h3>
+                <br>
+                <div class="title">Dimsum Crab terbuat dari 100% daging ayam dengan potongan crabstick tersedia dalam bentuk frozen food</div>
+                <div class="box">
+                    <div class="price">Rp.65.000</div>
+                    {{-- <button class="btn">Buy Now</button> --}}
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="sec">
+        <div class="products">
+
+            <div class="card">
+                <div class="img"><img src="{{Vite::asset('resources/img/Produk 3.jpeg')}}" alt=""></div>
+                <h3>Nugget Sayur 50 pcs</h3>
+                <br>
+                <div class="title">Nugget sayur sehat dengan isisan wortel, daun bawang, dandaging ayam dengan kualitas baik</div>
+                <div class="box">
+                    <div class="price">Rp.55.000</div>
+                    {{-- <button class="btn">Buy Now</button> --}}
+                </div>
+            </div>
+            <div class="card">
+                <div class="img"><img src="{{Vite::asset('resources/img/Produk 2.jpeg')}}" alt=""></div>
+                <h3>Nugget Ayam 50 pcs</h3>
+                <br>
+                <div class="title">Nugget ayam full daging ayam yang enak dan pastinya sehat</div>
+                <div class="box">
+                    <div class="price">Rp.55.000</div>
+                    {{-- <button class="btn">Buy Now</button> --}}
+                </div>
+            </div>
+            <div class="card">
+                <div class="img"><img src="{{Vite::asset('resources/img/Produk 1.jpeg')}}" alt=""></div>
+                <h3>Molen 10 pcs</h3>
+                <br>
+                <div class="title">Molen dengan isian pisang bisa custom isi tambahan seperti coklat atau keju</div>
+                <div class="box">
+                    <div class="price">Rp.30.000</div>
+                    {{-- <button class="btn">Buy Now</button> --}}
+                </div>
+            </div>
         </div>
     </section>
 
+
     <footer>
-        <p>Dirga yang buat anjay</p>
+        <p>Dirga yang buat anjay, ngeri banget loch</p>
     </footer>
 
     <script>
